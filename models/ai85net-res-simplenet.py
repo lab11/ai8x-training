459f356873950a2ce824ff8458faefda6d27c43f
Neal Jackson <nealsjackson@gmail.com>
2021-02-23 10:52:53 -0800

    try simplenet with fc

--- models/ai85net-res-simplenet.py
+++ models/ai85net-res-simplenet.py
@@ -79,6 +79,66 @@ class AI85ResidualSimpleNet(nn.Module):
         x = x.view(x.size(0), -1)
         return x
 
+class AI85ResidualSimpleNetFC(nn.Module):
+    """
+    Residual SimpleNet v1 Model
+    """
+    def __init__(
+            self,
+            num_classes=2,
+            num_channels=3,
+            dimensions=(224, 224),  # pylint: disable=unused-argument
+            bias=False,
+            **kwargs
+    ):
+        super().__init__()
+
+        self.conv1 = ai8x.FusedConv2dReLU(num_channels, 16, 3, stride=1, padding=1, bias=bias,
+                                          **kwargs)
+        self.conv2 = ai8x.FusedConv2dReLU(16, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.conv3 = ai8x.FusedConv2dReLU(20, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.conv4 = ai8x.FusedConv2dReLU(20, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.resid1 = ai8x.Add()
+        self.conv5 = ai8x.FusedMaxPoolConv2dReLU(20, 20, 3, pool_size=2, pool_stride=2,
+                                                 stride=1, padding=1, bias=bias, **kwargs)
+        self.conv6 = ai8x.FusedConv2dReLU(20, 20, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.resid2 = ai8x.Add()
+        self.conv7 = ai8x.FusedConv2dReLU(20, 44, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.conv8 = ai8x.FusedMaxPoolConv2dReLU(44, 48, 3, pool_size=2, pool_stride=2,
+                                                 stride=1, padding=1, bias=bias, **kwargs)
+        self.conv9 = ai8x.FusedConv2dReLU(48, 48, 3, stride=1, padding=1, bias=bias, **kwargs)
+        self.resid3 = ai8x.Add()
+        self.conv10 = ai8x.FusedMaxPoolConv2dReLU(48, 96, 3, pool_size=2, pool_stride=2,
+                                                  stride=1, padding=1, bias=bias, **kwargs)
+        self.conv11 = ai8x.FusedMaxPoolConv2dReLU(96, 512, 1, pool_size=2, pool_stride=2,
+                                                  padding=0, bias=bias, **kwargs)
+        self.conv12 = ai8x.FusedConv2dReLU(512, 128, 1, stride=1, padding=0, bias=bias, **kwargs)
+        self.conv13 = ai8x.FusedMaxPoolConv2dReLU(128, 128, 3, pool_size=2, pool_stride=14,
+                                                  stride=1, padding=1, bias=bias, **kwargs)
+        self.fc     = ai8x.Linear(128, num_classes, wide=True)
+
+    def forward(self, x):  # pylint: disable=arguments-differ
+        """Forward prop"""
+        x = self.conv1(x)          # 16x224x224
+        x_res = self.conv2(x)      # 20x224x224
+        x = self.conv3(x_res)      # 20x224x224
+        x = self.resid1(x, x_res)  # 20x224x224
+        x = self.conv4(x)          # 20x224x224
+        x_res = self.conv5(x)      # 20x112x112
+        x = self.conv6(x_res)      # 20x112x112
+        x = self.resid2(x, x_res)  # 20x112x112
+        x = self.conv7(x)          # 44x112x112
+        x_res = self.conv8(x)      # 48x56x56
+        x = self.conv9(x_res)      # 48x56x56
+        x = self.resid3(x, x_res)  # 48x56x56
+        x = self.conv10(x)         # 96x28x28
+        x = self.conv11(x)         # 512x14x14
+        x = self.conv12(x)         # 128x14x14
+        x = self.conv13(x)         # 128x1x1
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)         # num_classesx1x1
+        return x
+
 
 def ai85ressimplenet(pretrained=False, **kwargs):
     """
@@ -87,10 +147,22 @@ def ai85ressimplenet(pretrained=False, **kwargs):
     assert not pretrained
     return AI85ResidualSimpleNet(**kwargs)
 
+def ai85ressimplenetfc(pretrained=False, **kwargs):
+    """
+    Constructs a Residual SimpleNet v1 model.
+    """
+    assert not pretrained
+    return AI85ResidualSimpleNetFC(**kwargs)
+
 
 models = [
     {
         'name': 'ai85ressimplenet',
+        'min_input': 1,
+        'dim': 2,
+    },
+    {
+        'name': 'ai85ressimplenetfc',
         'min_input': 1,
         'dim': 2,
     },
--- scripts/train_vww_resnet18.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./train.py --epochs 500 --optimizer Adam --lr 0.00064 --compress schedule-cifar100-ressimplenet.yaml --model ai85resnet18 --dataset VisualWakeWord --device MAX78000 --batch-size 64 --print-freq 100 --validation-split .1 "$@"
